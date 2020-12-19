ef859305618cbad685f2a81c2f2d9f0d16dbe8b9
mat <lishay.matvey20001506matlish>
2020-12-19 16:04:17 +0300

    eslint fix

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,31 +1,34 @@
-import React from 'react'
+import React from 'react';
 
+// eslint-disable-next-line no-unused-vars
 const styles = {
-    content: {
-        fontSize: '35px',
-        marginTop: '20px',
-    }
-}
+	content: {
+		fontSize: '35px',
+		marginTop: '20px',
+	}
+};
 
+// eslint-disable-next-line no-unused-vars
 export default function Loading({text = 'Loading', speed = 100}) {
-    const [content, setContent] = React.useState(text)
+	// eslint-disable-next-line no-unused-vars
+	const [content, setContent] = React.useState(text);
 
-    return (
-        <div className='loading'>
-            <div className="loadingio-spinner-bean-eater-hnufo4ea5t9">
-                <div className="ldio-nmuh2ki4csa">
-                    <div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                    <div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className='loading'>
+			<div className="loadingio-spinner-bean-eater-hnufo4ea5t9">
+				<div className="ldio-nmuh2ki4csa">
+					<div>
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+					<div>
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
